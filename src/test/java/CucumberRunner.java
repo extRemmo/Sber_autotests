ad1a52f26f315d1e88b3cbdbb12349558ebd8b1d
extRemmo <dmitry.gra4ev@gmail.com>
2020-10-27 10:03:27 +0300

    cucumber tests2

---
+++ src/test/java/CucumberRunner.java
@@ -0,0 +1,10 @@
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (features = {"src/test/resources/features"},
+        plugin = {"util.AllureReporter"})
+public class CucumberRunner {
+
+}
