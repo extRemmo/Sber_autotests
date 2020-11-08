1dd7b0f56413ab8f6365d9cf39570aaf267a332e
Grachevskiy <51963148+extRemmo@users.noreply.github.com>
2020-11-08 18:44:03 +0300

    Update InsurePage.java
    Second part of SberTest

--- src/main/java/PObjects/InsurePage.java
+++ src/main/java/PObjects/InsurePage.java
@@ -5,12 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import steps.BaseSteps;
 
-public class InsurePage extends BasePageObject{
+
+public class InsurePage {
 
     @FindBy(xpath = "//div[@class='flexing']/div/a[contains(@href,'travel_ins')]")
     public WebElement onlineBtn;
@@ -21,8 +18,8 @@ public class InsurePage extends BasePageObject{
     @FindBy(xpath = "//ul[contains(@class, 'top-menu__list_center')]")
     WebElement closeBtn;
 
-    public InsurePage () {
-        PageFactory.initElements(BaseSteps.getDriver(), this);
+    public InsurePage (WebDriver driver) {
+        PageFactory.initElements(driver, this);
     }
 
     public void selectTravelMenu(String menuItem){
