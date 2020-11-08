dd83d9b5d99c6322fefd6cf01ac4818dc2b06fc6
Grachevskiy <51963148+extRemmo@users.noreply.github.com>
2020-11-08 18:47:30 +0300

    Update TravelOnlinePage.java

--- src/main/java/PObjects/TravelOnlinePage.java
+++ src/main/java/PObjects/TravelOnlinePage.java
@@ -5,23 +5,21 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import steps.BaseSteps;
 
 
-public class TravelOnlinePage extends BaseSteps {
 
-    @FindBy(xpath = "//a[@class='s-hero-banner__btn s-btn']")
+public class TravelOnlinePage  {
+
+    @FindBy(xpath = "//div[@class='s-hero-banner__content']")
     public WebElement applyOnlineButton;
 
 
-    public TravelOnlinePage () {
-        PageFactory.initElements(BaseSteps.getDriver(), this);
+    public TravelOnlinePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
     }
 
     public void selectTravelOnline(String menuItem) {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='s-hero-banner__btn s-btn']")));
-        applyOnlineButton.findElement(By.xpath("//a[@class='s-hero-banner__btn s-btn' and contains(text(),'"+menuItem+"')]")).click();
+        applyOnlineButton.findElement(By.xpath(".//a[contains(text(),'"+menuItem+"')]")).click();
     }
 }
 
