8f42a49144022424c697c9ad87b964c2cba6650d
Grachevskiy <51963148+extRemmo@users.noreply.github.com>
2020-11-08 18:45:01 +0300

    Update MainPage.java
    Second part of SberTest

--- src/main/java/PObjects/MainPage.java
+++ src/main/java/PObjects/MainPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import steps.BaseSteps;
+
 
 public class MainPage {
 
@@ -15,8 +15,8 @@ public class MainPage {
     @FindBy(xpath = "//li[contains(@class, 'kitt-top-menu__item_opened')]")
     WebElement subMenu;
 
-    public MainPage() {
-        PageFactory.initElements(BaseSteps.getDriver(), this);
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
     }
 
     public void selectMainMenu(String menuItem){
