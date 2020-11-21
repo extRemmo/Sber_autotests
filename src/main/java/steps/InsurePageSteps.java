bc2c5f782d9be196bd2ca5e40a8e0434302f5272
Grachevskiy <51963148+extRemmo@users.noreply.github.com>
2020-11-21 16:19:26 +0300

    Update InsurePageSteps.java

--- src/main/java/steps/InsurePageSteps.java
+++ src/main/java/steps/InsurePageSteps.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertTrue;
 public class InsurePageSteps {
     @Step("заголовок страницы - Страхование равен {0}")
     public void checkPageTitle(String expectedTitle){
-        String actualTitle = new InsurePage().title.getText();
+       String actualTitle = new InsurePage().title.getText().replaceAll("\n", " ");
         assertTrue(String.format("Заголовок равен [%s]. Ожидалось - [%s]",
                 actualTitle, expectedTitle), actualTitle.contains(expectedTitle));
     }
